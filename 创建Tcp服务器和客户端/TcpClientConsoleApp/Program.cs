5ca02035b93db366af2fe1e141494479f4caa74d
jadePureOne <jadePureOne@outlook.com>
2025-08-28 14:20:15 +0800

    ✨ feat:
    创建tcp服务器和客户端

---
+++ 创建Tcp服务器和客户端/TcpClientConsoleApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using TouchSocket.Core;
+using TouchSocket.Sockets;
+
+var tcpClient = new TcpClient();
+
+tcpClient.Connecting = async (client, e) =>
+{
+    Console.WriteLine($"服务端{client.IP}连接中...");
+
+    await Task.CompletedTask;
+};
+
+tcpClient.Connected = async (client, e) =>
+{
+    //客户端连接成功
+    Console.WriteLine($"服务端{client.IP}连接成功");
+    await Task.CompletedTask;
+};
+
+//有客户端正在断开连接，只有当主动断开时才有效。
+tcpClient.Closing = (client, e) =>
+{
+    Console.WriteLine($"服务端{client.IP}断开中...");
+    return EasyTask.CompletedTask;
+};
+tcpClient.Closed = (client, e) =>
+{
+    Console.WriteLine($"服务端{client.IP}断开连接");
+    return EasyTask.CompletedTask;
+};
+
+tcpClient.Received = async (client, e) =>
+{
+    //从客户端收到信息
+    var mes = e.ByteBlock.Span.ToString(Encoding.UTF8);
+
+    Console.WriteLine($"已从{client.IP}接收到信息：{mes}");
+    await Task.CompletedTask;
+};
+
+try
+{
+    await tcpClient.ConnectAsync("127.0.0.1:7789");//调用连接，当连接不成功时，会抛出异常。
+
+    Result result = await tcpClient.TryConnectAsync();//或者可以调用TryConnectAsync
+    if (result.IsSuccess)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            await tcpClient.SendAsync(Encoding.UTF8.GetBytes($"这是第{i}条消息"));
+            await Task.Delay(1000);
+        }
+
+        await tcpClient.CloseAsync();
+        Console.ReadLine();
+    }
+}
+catch (Exception)
+{
+    throw;
+}
--- 创建Tcp服务器和客户端/TcpServerConsoleApp/Program.cs
+++ 创建Tcp服务器和客户端/TcpServerConsoleApp/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using TouchSocket.Core;
+using TouchSocket.Sockets;
+
+var tcpService = new TcpService();
+
+//tcpService.Connecting = async (client, e) =>
+//{
+//    //可进行连接验证，验证不通过可取消连接
+//    if (client.IP == "127.0.0.1")
+//    {
+//        //拒绝本地连接
+//        e.IsPermitOperation = false;
+//        e.Message = "拒绝本地连接";
+//        Console.WriteLine($"拒绝本地连接");
+//    }
+//    await Task.CompletedTask;
+//};
+
+tcpService.Connected = async (client, e) =>
+{
+    //客户端连接成功
+    Console.WriteLine($"客户端{client.IP}连接成功");
+    await Task.CompletedTask;
+};
+
+//有客户端正在断开连接，只有当主动断开时才有效。
+tcpService.Closing = (client, e) =>
+{
+    Console.WriteLine($"客户端{client.IP}断开中...");
+    return EasyTask.CompletedTask;
+};
+tcpService.Closed = (client, e) =>
+{
+    Console.WriteLine($"客户端{client.IP}断开连接");
+    return EasyTask.CompletedTask;
+};
+
+tcpService.Received = async (client, e) =>
+{
+    var byteBlock = e.ByteBlock; //收到的数据是字节块
+    var bytes = byteBlock.ToArray(); //转换为字节数组
+    var text = Encoding.UTF8.GetString(bytes); //转换为字符串
+
+    //从客户端收到信息
+    var mes = e.ByteBlock.Span.ToString(Encoding.UTF8);
+
+    Console.WriteLine($"已从{client.IP}接收到信息：{mes}");
+
+    //回复客户端
+    await client.SendAsync($"服务器已收到：{byteBlock.Memory}");
+
+    await Task.CompletedTask;
+};
+
+await tcpService.StartAsync(7789);//启动
+
+Console.WriteLine("服务器已启动，等待客户端连接...");
+
+while (true)
+{
+    var input = Console.ReadLine();
+    if (input == "exit")
+    {
+        break;
+    }
+}
